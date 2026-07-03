fa13d1fddc38df7dc39e6becd6160b72cacba208
nagasai37 <23ht1a0537@gmail.com>
2026-07-03 15:04:23 +0530

    javascript coding

---
+++ day36/syncawit.js
@@ -0,0 +1,13 @@
+let loginpromis=new
+promise((resolve,reject)=>{
+    setTimeout(()=>{
+        const login=user 
+        
+
+    })
+    
+    reject("network error");
+});
+promise1.catch((error)=>{
+    console.log(error)
+});
